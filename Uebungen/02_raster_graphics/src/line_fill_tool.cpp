97b23d132f6aea89b3e7ba0335c67f43d458e79f
TheT87 <45598431+TheT87@users.noreply.github.com>
2019-05-23 16:23:18 +0200

    testshape fillable

--- Uebungen/02_raster_graphics/src/line_fill_tool.cpp
+++ Uebungen/02_raster_graphics/src/line_fill_tool.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 
 // Initialize the tool and store a reference of a canvas_buffer
-line_fill_tool::line_fill_tool(canvas_buffer& canvas) : tool_base(canvas)
+line_fill_tool::line_fill_tool(canvas_buffer& canvas): tool_base(canvas)
 {
 	// This tool has no shape and is not draggable
 	shape = TS_NONE;
@@ -22,48 +22,69 @@ void line_fill_tool::draw(int x, int y)
 {
 	/************
 	Additional task: Implement the line fill algorithm. You can declare
-					 helper methods and properties in the header file if needed and
+	                 helper methods and properties in the header file if needed and
 					 implement them here.
-	Zusatzaufgabe:   Implementieren Sie den Line-Fill-Algorithmus. Falls benoetigt
-					 koennen Sie sich Helfermethoden in der dazugehoerigen Header-Datei
+    Zusatzaufgabe:   Implementieren Sie den Line-Fill-Algorithmus. Falls benoetigt
+	                 koennen Sie sich Helfermethoden in der dazugehoerigen Header-Datei
 					 deklarieren und in dieser Datei definieren.
 	*************/
-
-	std::cout << "Bevore drawing pixels" << endl;
-
-	if (!canvas.get_pixel(x - 1, y) && (x > 0 && x < canvas.get_width() - 1))
-	{
-		canvas.set_pixel(x - 1, y);
-
-		if ((0 < y < canvas.get_height() - 1) && !canvas.get_pixel(x - 1, y - 1))
+	
+	canvas.set_pixel(x, y);
+		if (!canvas.get_pixel(x - 1, y) && (x > 0 && x < canvas.get_width() - 1))
 		{
-			draw(x, y - 1);
+			draw(x - 1, y);
+		}
+		else
+		{
+			if (!canvas.get_pixel(x, y + 1))
+			{
+				draw(x, y + 1);
+			}
+			if (!canvas.get_pixel(x, y - 1))
+			{
+				draw(x, y - 1);
+			}
+			else
+			{
+				if (!canvas.get_pixel(x, y - 1))
+				{
+					draw(x, y - 1);
+				}
+			}
 		}
 
-		if ((0 < y < canvas.get_height() - 1) && !canvas.get_pixel(x - 1, y + 1))
+		if (!canvas.get_pixel(x + 1, y) && (x > 0 && x < canvas.get_width() - 1))
 		{
-			draw(x, y + 1);
+			draw(x + 1, y);
+		}
+		else
+		{
+			if (!canvas.get_pixel(x, y + 1))
+			{
+				draw(x, y + 1);
+			}
+			if (!canvas.get_pixel(x, y - 1))
+			{
+				draw(x, y - 1);
+			}
+			else
+			{
+				if (!canvas.get_pixel(x, y - 1))
+				{
+					draw(x, y - 1);
+				}
+			}
 		}
 
-		draw(x - 1, y);
-	}
+		
+		
 
-	if (!canvas.get_pixel(x + 1, y) && (x > 0 && x < canvas.get_width() - 1))
-	{
-		canvas.set_pixel(x + 1, y);
 
-		if ((0 < y < canvas.get_height() - 1) && !canvas.get_pixel(x + 1, y - 1))
-		{
-			draw(x, y - 1);
-		}
+	
+	
 
-		if ((0 < y < canvas.get_height() - 1) && !canvas.get_pixel(x + 1, y + 1))
-		{
-			draw(x, y + 1);
-		}
 
-		draw(x + 1, y);
-	}
+
 
 }
 
@@ -73,7 +94,7 @@ void line_fill_tool::draw(int x, int y)
 
 // Put debug output into the stream "stream" to be displayed in the
 // main window
-void line_fill_tool::set_text(stringstream & stream)
+void line_fill_tool::set_text(stringstream& stream)
 {
-	stream << "Tool: Line Fill (click to fill)";
+	stream<<"Tool: Line Fill (click to fill)";
 }
