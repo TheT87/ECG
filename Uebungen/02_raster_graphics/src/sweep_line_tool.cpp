7ef6fdaa737c4cfd193d6a56e7ad5e9a1dd60b30
TheT87 <45598431+TheT87@users.noreply.github.com>
2019-05-23 22:10:29 +0200

    triangle partly working

--- Uebungen/02_raster_graphics/src/sweep_line_tool.cpp
+++ Uebungen/02_raster_graphics/src/sweep_line_tool.cpp
@@ -1,15 +1,16 @@
 #include "sweep_line_tool.h"
-
+#include <algorithm>
 
 sweep_line_tool	::sweep_line_tool(canvas_buffer & canvas) : tool_base(canvas)
 	{
 		shape = TS_NONE;
 	}
 
+void sweep_line_tool::draw(int x0, int y0, int x1, int y1)
+{
 
-/*void sweep_line_tool::draw(int x0, int y0, int x1, int y1, int x2, int y2) {
+}
 
-}*/
 
 
 void sweep_line_tool::set_text(std::stringstream& stream) 
